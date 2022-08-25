c1eeaaca5b1d618b333f20326a1f06073ca6ba65
임동균 <70128434+Dong-Es@users.noreply.github.com>
2022-08-26 06:19:16 +0900

    Add files via upload

---
+++ Alerts/problem/main.js
@@ -0,0 +1,6 @@
+function change(text){
+    document.getElementById('alerttext').style.display='block';
+    document.getElementById('alerttext').innerHTML=text;
+}
+change('이것은 A요')
+change('이것은 B요')
